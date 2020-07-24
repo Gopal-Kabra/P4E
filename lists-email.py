5059475355cf933b01e9a7f0bf71cea411c18ebb
Gopal-Kabra <68639366+Gopal-Kabra@users.noreply.github.com>
2020-07-24 10:31:58 +0530

    Update lists-email.py

--- lists-email.py
+++ lists-email.py
@@ -8,8 +8,11 @@ for line in fh :
    line = line.rstrip()
    if line.startswith('From ') :
         words = line.split()
-        new1 = words[1]
-        print(new1)
-        count+=1       
-
+        for w in words :
+            if '@' in w:
+                new1 = w
+                print(new1)
+                count+=1
+            
 print("There were", count, "lines in the file with From as the first word")
+
